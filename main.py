f53ae0294170d80fe5f4de6445486574842cb5b2
artem <dymov.artem@outlook.com>
2022-01-24 22:39:45 +0200

    base functionality

--- main.py
+++ main.py
@@ -11,8 +11,17 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
-    await postgres.main()
+    users = await postgres.main()
+
+    if message.from_user.id in users:
+        #Кидаем здесь нужный стейт
+        print("Вы есть в базе данных")
+    else:
+        await message.answer("Привет! Я бот, который отправляет поздравления другим людям!")
+        print("Вас нет в базе данных")
+        await postgres.create(message.from_user.id)
+
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- postgres.py
+++ postgres.py
@@ -8,19 +8,37 @@ class User(db.Model):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.Unicode(), default='nonamer')
-    username_type = db.Column(db.Unicode(), default='noname')
-    message_for_user = db.Column(db.Unicode(), default='message')
+    username = db.Column(db.Unicode(), default='empty')
+    username_type = db.Column(db.Unicode(), default='empty')
+    message_for_user = db.Column(db.Unicode(), default='empty')
 
 
 
 async def main():
     await db.set_bind(data.host)
     await db.gino.create_all()
+    '''
+
+    async def create(id, username, username_type, message_for_user):
+        user = await User.create(id=id,username=username, username_type=username_type, message_for_user=message_for_user)
+
+    await create(id, username, username_type, message_for_user)
+'''
+    s = []
+    async with db.transaction():
+        async for row in User.select('id', 'username', 'username_type', 'message_for_user').gino.iterate():
+            s.append(row[0])
+
+
 
     # further code goes here
 
     await db.pop_bind().close()
+    return s
+
+async def create(id):
+    await db.set_bind(data.host)
+    user = await User.create(id=id)
 
 
 asyncio.get_event_loop().run_until_complete(main())
