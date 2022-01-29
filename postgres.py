44c8d78fb8648ac695d1fae451684e9626e34841
artem <dymov.artem@outlook.com>
2022-01-29 19:21:05 +0200

    minor update

--- postgres.py
+++ postgres.py
@@ -4,32 +4,32 @@ import data
 
 db = Gino()
 
+#Модели пихаем в 1 файл
 class User(db.Model):
     __tablename__ = 'messages'
 
-    id = Column(db.Integer, Sequence("user_id_seq"), primary_key=True)
-    tg_id = Column(db.Integer)
-    username = Column(db.String(200))
-    firstname = Column(db.String(200))
-    lastname = Column(db.String(200))
-    fullname = Column(db.String(200))
-    is_blocked_by_bot = Column(db.Boolean)
-    is_bot_blocked = Column(db.Boolean)
-    language = Column(db.String(200))
-    is_admin = Column(db.Boolean)
+    id = db.Column(db.Integer, db.Sequence("user_id_seq"), primary_key=True)
+    tg_id = db.Column(db.Integer)
+    username = db.Column(db.String(200))
+    firstname = db.Column(db.String(200))
+    lastname = db.Column(db.String(200))
+    fullname = db.Column(db.String(200))
+    is_blocked_by_bot = db.Column(db.Boolean)
+    is_bot_blocked = db.Column(db.Boolean)
+    language = db.Column(db.String(200))
+    is_admin = db.Column(db.Boolean)
 
 
-
+'''
 async def main():
     await db.set_bind(data.host)
     await db.gino.create_all()
-    '''
 
     async def create(id, username, username_type, message_for_user):
         user = await User.create(id=id,username=username, username_type=username_type, message_for_user=message_for_user)
 
     await create(id, username, username_type, message_for_user)
-'''
+
     s = []
     async with db.transaction():
         async for row in User.select('id', 'username', 'username_type', 'message_for_user').gino.iterate():
@@ -41,8 +41,8 @@ async def main():
 
     await db.pop_bind().close()
     return s
-
-
+'''
+#Подобные функции пихаем во 2 файл
 async def get_users():
     users = await User.query.gino.all()
     return users
