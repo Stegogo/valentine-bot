032350d77812b4e718c9c2baaf6729d6466c61e8
bellzebul <butkov926@gmail.com>
2022-02-13 19:00:24 +0300

    minor.update_final4

---
+++ data.py
@@ -0,0 +1,8 @@
+host = "postgresql://ayjdkqmplfuqrm:12c98bfad6f20dd0438fa14ca1c00efa842a07bd61a937e5c55d60738b67f42a@ec2-54-247-158-179.eu-west-1.compute.amazonaws.com:5432/dae0d365n6fjro"
+
+token = '5145738428:AAGYLxquo_YtDQ3jArFGolyBBHjj90CBNFU'
+
+moder_chat_id = -1001749130800
+userbot_id = 838524205
+
+
